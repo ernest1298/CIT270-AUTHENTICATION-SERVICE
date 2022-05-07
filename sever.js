ec86af58328c9044a454d434c14e006b9401d49b
ernest1298 <97247914+ernest1298@users.noreply.github.com>
2022-05-07 15:15:11 -0600

    changes

---
+++ sever.js
@@ -0,0 +1,8 @@
+const { response } = require('express');
+const express = require('express');//import the library 
+
+const app = express(); //use the library
+
+app.listen(3000,()=>{console.log("listening...")});//listen
+
+app.get('/',(request,response)=>{response.send("Hello")});//respond
